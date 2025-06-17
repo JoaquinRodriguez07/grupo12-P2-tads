c057ecf0c97ee7bd889f297fb6a362ab57c0df3a
mliceran <nasza24yt@gmail.com>
2025-06-17 10:34:08 -0300

    cree el menu en el main. borre la prueba de la carga de datos y me olvide de meterla de nuevo, guarda con eso.

--- src/main/java/um/edu/uy/Main.java
+++ src/main/java/um/edu/uy/Main.java
@@ -1,46 +1,97 @@
 package um.edu.uy;
 
-import um.edu.uy.clases.Peliculas;
-import um.edu.uy.cargadoresDeData.CargadorDeMovies;
-import um.edu.uy.tadsAuxiliares.hashtable.HashCerradaLineal;
-import um.edu.uy.tadsAuxiliares.hashtable.HashTable;
+import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        CargadorDeMovies loader = new CargadorDeMovies();
-        String csvFile = "movies_metadata.csv";
+        Scanner scanner = new Scanner(System.in);
+        boolean cierre = true;
 
-        System.out.println("Iniciando carga de películas...");
-        long startTime = System.currentTimeMillis();
+        while (cierre) {
+            //renzo me dijo que lo haga lo mas parecido... puse los mismos espacios por si acaso
+            System.out.println("Menu principal");
+            System.out.println("       Seleccione la opción que desee:");
+            System.out.println("    1. Carga de datos");
+            System.out.println("    2. Ejecutar consultas");
+            System.out.println("    3. Salir");
+            String respuesta = scanner.nextLine(); //esto hace que escribas en la siguiente linea
+            switch (respuesta) {
+                case "1":
+                    //aca iria lo de cargar los datos. mientras tanto voy a poner un print para saber que funciona
+                    System.out.println("Carga de datos exitosa, tiempo de ejecución de la carga: <aca va el tiempo de ejecución>");
 
-        HashTable<String, Peliculas> peliculasHash = loader.cargadorMoviesAHash(csvFile);
+                    break;
 
-        long endTime = System.currentTimeMillis();
-        System.out.println("--- Carga Finalizada ---");
-        System.out.println("Total de películas cargadas en la tabla hash: " + peliculasHash.tamanio());
-        System.out.println("Tiempo total de carga: " + (endTime - startTime) + " ms");
+                case "2":
+                    boolean cierre2 = true;
+                    while (cierre2) {
+                        System.out.println("1. Top 5 de las películas que más calificaciones por idioma.");
+                        System.out.println("2. Top 10 de las películas que mejor calificación media tienen por parte de los usuarios.");
+                        System.out.println("3. Top 5 de las colecciones que más ingresos generaron.");
+                        System.out.println("4. Top 10 de los directores que mejor calificación tienen.");
+                        System.out.println("5. Actor con más calificaciones recibidas en cada mes del año.");
+                        System.out.println("6. Usuarios con más calificaciones por género");
+                        System.out.println("7. Salir");
+                        String respuesta2 = scanner.nextLine();
+                        switch (respuesta2) {
+                            case "1":
+                                //funcion. de mientras pongo algo.
+                                System.out.println("funcion 1");
 
+                                break;
 
-        String testId = "9263";
-        Peliculas testMovie = peliculasHash.obtener(testId); //aca me fijo si esta
+                            case "2":
+                                //funcion 2. de mientras pongo algo.
+                                System.out.println("funcion 2");
 
-        if (testMovie != null) {
-            System.out.println("\nPelícula encontrada con ID " + testId + ":");
-            System.out.println(testMovie);
-        } else {
-            System.out.println("\nPelícula con ID " + testId + " no encontrada.");
+                                break;
+
+                            case "3":
+                                //funcion 3. de mientras pongo algo.
+                                System.out.println("funcion 3");
+
+                                break;
+
+                            case "4":
+                                //funcion 4. de mientras pongo algo.
+                                System.out.println("funcion 4");
+
+                                break;
+
+                            case "5":
+                                //funcion 5. de mientras pongo algo.
+                                System.out.println("funcion 5");
+
+                                break;
+
+                            case "6":
+                                //funcion 6. de mientras pongo algo.
+                                System.out.println("funcion 6");
+
+                                break;
+
+                            case "7":
+                                //preguntarle a renzo si tiene que terminar este menu y volver al anterior o terminar ambos menus.
+                                //por ahora lo deje en que terminara este menu y regresara al otro.
+                                cierre2 = false;
+                                break;
+
+                            default:
+                                System.out.println("Opción no valida. Intente de nuevo.");
+                                break;
+                        }
+                    }
+
+                    break;
+
+                case "3":
+                    cierre = false;
+                    break;
+
+                default:
+                    System.out.println("Opción no valida. Intente de nuevo.");
+                    break;
+            }
         }
-
-        String IdTrucho = "99999999";
-        Peliculas noExisteId = peliculasHash.obtener(IdTrucho); //a ver si me tira excepcion con una que no funca
-
-        if (noExisteId != null) {
-            System.out.println("\nPelícula encontrada con ID " + IdTrucho + ":");
-            System.out.println(noExisteId);
-        } else {
-            System.out.println("\nPelícula con ID " + IdTrucho + " no encontrada.");
-        }
-
-        System.out.println(peliculasHash);
     }
 }
